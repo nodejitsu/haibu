2a4b2be860755fec15e40ec6aff90ac625e4f44d
indexzero <charlie.robbins@gmail.com>
2011-05-29 00:33:03 -0400

    [refactor minor] Style updates: `function()` should be `function ()`

--- test/fixtures/repositories/npm-deps/index.js
+++ test/fixtures/repositories/npm-deps/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express.createServer();
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get('/', function (req, res) {
+  res.send('Hello World');
 });
 
 app.listen(3000);
