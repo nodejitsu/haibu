4ecb0ec8b68f5dfc911e1d153a6f7c508ee74059
indexzero <charlie.robbins@gmail.com>
2011-08-27 22:57:23 -0400

    [minor] Minor updates to logger plugin and associated tests

--- test/plugins/logger-test.js
+++ test/plugins/logger-test.js
@@ -1,23 +1,20 @@
 /*
- * model.js: Tests for the 'model' module.
+ * logger-test.js: Tests for the haibu `logger` plugin.
  *
  * (C) 2010, Nodejitsu Inc.
  *
  */
 
 var assert = require('assert'),
-    exec = require('child_process').exec,
-    path = require('path'),
-    eyes = require('eyes'),
-    winston = require('winston'),
     vows = require('vows'),
     haibu = require('../../lib/haibu'),
     helpers = require('../helpers');
 
 vows.describe('haibu/plugins/logger').addBatch(helpers.requireInit()).addBatch({
-  "When using the log module": {
+  "When using the logger plugin": {
     "it should have the correct methods defined": function () {
       assert.isFunction(haibu.plugins.logger.init);
+      assert.isFunction(haibu.plugins.logger.initInput);
     }
   }
 }).export(module);
