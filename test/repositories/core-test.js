2fa1b25de116e65b26792d262deb33c0e2e20bd0
indexzero <charlie.robbins@gmail.com>
2011-09-11 03:09:40 -0400

    [test fix refactor] Simplify the tests significantly for non-async operations. Update tests so that they assert.throw instead of assert.instanceOf(something, Error)

--- test/repositories/core-test.js
+++ test/repositories/core-test.js
@@ -14,174 +14,168 @@ var assert = require('assert'),
     haibu = require('../../lib/haibu');
 
 var app = {
-      "name": "test",
-      "user": "marak",
-      "repository": {
-        "type": "create",
-        "url": "https://github.com/Marak/hellonode.git",
-      },
-      "scripts": {
-        "start": "server.js"
-      }
-    },
-    app2 = {
-      "name": "test2",
-      "user": "marak",
-      "repository": {
-        "type": "create",
-        "url": "https://github.com/Marak/hellonode.git",
-      },
-      "scripts": {
-        "start": "server.js"
-      }
-    },
-    appMalformed = {
-    };
-
-function newRepo(app, options) {
+  "name": "test",
+  "user": "marak",
+  "repository": {
+    "type": "create",
+    "url": "https://github.com/Marak/hellonode.git",
+  },
+  "scripts": {
+    "start": "server.js"
+  }
 };
 
-vows.describe('haibu/repositories/core').addBatch(
-  helpers.requireInit()
-).addBatch({
-  "When using haibu, ": {
-    "get the haibu.repository module": {
+var app2 = {
+  "name": "test2",
+  "user": "marak",
+  "repository": {
+    "type": "create",
+    "url": "https://github.com/Marak/hellonode.git",
+  },
+  "scripts": {
+    "start": "server.js"
+  }
+};
+
+var appMalformed = {};
+
+function newRepo(app, options) { }
+
+vows.describe('haibu/repositories/core').addBatch(helpers.requireInit()).addBatch({
+  "When using the repository module": {
+    "should have store functions": function () {
+      assert.isObject(haibu.repository);
+      assert.isFunction(haibu.repository.create);
+      assert.isFunction(haibu.repository.add);
+      assert.isFunction(haibu.repository.remove);
+      assert.isFunction(haibu.repository.list);
+      assert.isFunction(haibu.repository.validate);
+    },
+    "the add() method": {
+      "should add a new repo type": function () {
+        haibu.repository.add('test', newRepo);
+        assert.include(haibu.repository.list(), 'test');
+      },
+      "with existing repository type": {
+        "should throw an error": function () {
+          assert.throws(function () {
+            haibu.repository.add('test', newRepo);
+          });
+        }
+      }
+    },
+    "the list() method": {
+      topic: haibu.repository.list(),
+      "should list all repo types": function (list) {
+        assert.isArray(list);
+        assert.include(list, 'git');
+        assert.include(list, 'tar');
+        assert.include(list, 'zip');
+        assert.include(list, 'local');
+      }
+    },
+    "the remove() method": {
       topic: function () {
-        return haibu.repository;
+        haibu.repository.add('test2', newRepo);
+        return haibu.repository.remove('test2')
+          ? false
+          : haibu.repository.list();
       },
-      "should have store functions": function (repo) {
-        assert.isObject(repo);
-        assert.isFunction(repo.create);
-        assert.isFunction(repo.add);
-        assert.isFunction(repo.remove);
-        assert.isFunction(repo.list);
-        assert.isFunction(repo.validate);
+      "should remove a repo type": function (list) {
+        assert.isArray(list);
+        assert.equal(list.indexOf('test2'), -1);
+      }
+    },
+    "the create() method": {
+      topic: function () {
+        haibu.repository.add('create', newRepo);
+        return haibu.repository.create(app, {});
       },
-      "executing the add() method": {
-        topic: function (repo) {
-          this.callback(repo.add('test', newRepo));
+      "should return a repo instance": function (repo) {
+        assert.instanceOf(repo, newRepo);
+      },
+      "with a malformed app definition": {
+        "should return an Error object": function (err, repoTest) {
+          assert.throws(function () {
+            repo.create(appMalformed, {});
+          });
+        }
+      }
+    }
+  },
+  "an instance of the Repository class": {
+    topic: function (repo) {
+      return new haibu.repository.Repository(app2, { appsDir: '/this/is/a/path'});
+    },
+    "should return a Repository instance": function (repo) {
+      assert.instanceOf(repo, haibu.repository.Repository);
+      assert.isFunction(repo.validate);
+      assert.isFunction(repo.installDependencies);
+      assert.isFunction(repo.stat);
+      assert.isFunction(repo.mkdir);
+      assert.isFunction(repo.bootstrap);
+      assert.equal(repo.userDir, path.join('/this/is/a/path', app2.user));
+      assert.equal(repo.appDir, path.join('/this/is/a/path', app2.user, app2.name));
+      repo._setHome('home1');
+      assert.equal(repo.homeDir, path.join('/this/is/a/path', app2.user, app2.name, 'home1'));
+    },
+    "executing the mkdir() method": {
+      topic: function (repo, repositories) {
+        self = this;
+        repo.appsDir = haibu.config.get('directories:apps');
+        exec('rm -rf ' + path.join(repo.appDir, '*'), function(err) {
+          repo.mkdir(self.callback);
+        })
+      },
+      "should create the apps directory": function (err, ready) {
+        assert.isTrue(ready);
+        try {
+          assert.isNotNull(fs.statSync(path.join(haibu.config.get('directories:apps'), app2.user, app2.name)));
+        } 
+        catch (ex) {
+          // If this operation fails, fail the test
+          assert.isNull(ex);
+        }
+      },
+      "plus the installDependencies() method with package.json": {
+        topic: function (ready, repo, repositories) {
+          self = this;
+          exec('cp -r ' + path.join(__dirname, '..', 'fixtures', 'repositories', 'local-file-dependencies') + ' ' + repo.homeDir, function (err) {
+            if (err) {
+              return self.callback(err);
+            }
+            
+            repo.installDependencies(self.callback);
+          });
         },
-        "should add a new repo type": function (err) {
-        },
-        "with existing repository type": {
-          topic: function (repo) {
-            this.callback(null, repo.add('test', newRepo));
-          },
-          "should return an Error object": function (err, repoTest) {
-              assert.instanceOf(repoTest, Error);
+        "should install the 'color' package at the correct location": function (err, packages) {
+          assert.isArray(packages);
+          try {
+            assert.isNotNull(fs.statSync(path.join(haibu.config.get('directories:apps'), app2.user, app2.name, 'home1', 'node_modules', 'color')));
+          } 
+          catch (ex) {
+            // If this operation fails, fail the test
+            assert.isNull(ex);
           }
-        }
-      },
-      "executing the list() method": {
-        topic: function (repo) {
-          return repo.list();
         },
-        "should list all repo types": function (list) {
-          assert.isArray(list);
-          assert.include(list, 'git');
-          assert.include(list, 'tar');
-          assert.include(list, 'zip');
-          assert.include(list, 'local');
-        }
-      },
-      "executing the remove() method": {
-        topic: function (repo) {
-          repo.add('test2', newRepo);
-          if (repo.remove('test2')) return false;
-          return repo.list();
-        },
-        "should remove a repo type": function (list) {
-            assert.isArray(list);
-            assert.equal(list.indexOf('test2'), -1);
-        }
-      },
-      "executing the create() method": {
-        topic: function (repo) {
-          repo.add('create', newRepo);
-          return repo.create(app, {});
-        },
-        "should return a repo instance": function (repo) {
-          assert.instanceOf(repo, newRepo);
-        },
-        "with a malformed app definition": {
-          topic: function (newRepo, repo) {
-            return repo.create(appMalformed, {});
+        "and also with app.dependencies": {
+          topic: function (packages, ready, repo, repositories) {
+            repo._setHome('home2');
+            repo.app.dependencies = {"color": "x.x.x"};
+            repo.installDependencies(this.callback);
           },
-          "should return an Error object": function (err, repoTest) {
-              assert.instanceOf(repoTest, Error);
-          }
-        }
-      },
-      "getting an instance of the Repository class": {
-        topic: function (repo) {
-          return new repo.Repository(app2, { appsDir: '/this/is/a/path'});
-        },
-        "should return a Repository instance": function (repo) {
-          assert.instanceOf(repo, haibu.repository.Repository);
-          assert.isFunction(repo.validate);
-          assert.isFunction(repo.installDependencies);
-          assert.isFunction(repo.stat);
-          assert.isFunction(repo.mkdir);
-          assert.isFunction(repo.bootstrap);
-          assert.equal(repo.userDir, path.join('/this/is/a/path', app2.user));
-          assert.equal(repo.appDir, path.join('/this/is/a/path', app2.user, app2.name));
-          repo._setHome('home1');
-          assert.equal(repo.homeDir, path.join('/this/is/a/path', app2.user, app2.name, 'home1'));
-        },
-        "executing the mkdir() method": {
-          topic: function (repo, repositories) {
-            self = this;
-            repo.appsDir = haibu.config.get('directories:apps');
-            exec('rm -rf ' + path.join(repo.appDir, '*'), function(err) {
-              repo.mkdir(self.callback);
-            })
-          },
-          "should create the apps directory": function (err, ready) {
-            assert.isTrue(ready);
+          "should install the 'color' package at the correct location": function (err, packages) {
+            assert.isArray(packages);
             try {
-              assert.isNotNull(fs.statSync(path.join(haibu.config.get('directories:apps'), app2.user, app2.name)));
-            } catch (ex) {
+              assert.isNotNull(fs.statSync(path.join(haibu.config.get('directories:apps'), app2.user, app2.name, 'home2', 'node_modules', 'color')));
+            } 
+            catch (ex) {
               // If this operation fails, fail the test
               assert.isNull(ex);
             }
-          },
-          "plus the installDependencies() method with package.json": {
-            topic: function (ready, repo, repositories) {
-              self = this;
-              exec('cp -r ' + path.join(__dirname, '..', 'fixtures', 'repositories', 'local-file-dependencies') + ' ' + repo.homeDir, function (err) {
-                if (err) self.callback(err);
-                repo.installDependencies(self.callback);
-              });
-            },
-            "should install the 'color' package at the correct location": function (err, packages) {
-              assert.isArray(packages);
-              try {
-                assert.isNotNull(fs.statSync(path.join(haibu.config.get('directories:apps'), app2.user, app2.name, 'home1', 'node_modules', 'color')));
-              } catch (ex) {
-                // If this operation fails, fail the test
-                assert.isNull(ex);
-              }
-            },
-            "and also with app.dependencies": {
-              topic: function (packages, ready, repo, repositories) {
-                repo._setHome('home2');
-                repo.app.dependencies = {"color": "x.x.x"};
-                repo.installDependencies(this.callback);
-              },
-              "should install the 'color' package at the correct location": function (err, packages) {
-                assert.isArray(packages);
-                try {
-                  assert.isNotNull(fs.statSync(path.join(haibu.config.get('directories:apps'), app2.user, app2.name, 'home2', 'node_modules', 'color')));
-                } catch (ex) {
-                  // If this operation fails, fail the test
-                  assert.isNull(ex);
-                }
-              }
-            } 
-          } 
-        }
-        // TODO: add tests for all Repository class functions!!
-      }
+          }
+        } 
+      } 
     }
   }
 }).export(module);
